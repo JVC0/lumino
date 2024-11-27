2c77bd1a341f5aec36415c61327c82b1d6c7cdfe
Kai <145053705+YoooKai@users.noreply.github.com>
2024-11-27 12:22:28 +0000

    Update models.py

--- subjects/models.py
+++ subjects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Subject(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="taught_subjects")  
+    students = models.ManyToManyField(settings.AUTH_USER_MODEL, through='users.Enrollment', related_name="enrolled_subjects")  
+
+    def __str__(self):
+        return self.code
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    subject = models.ForeignKey('subjects.Subject', related_name='lessons', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
